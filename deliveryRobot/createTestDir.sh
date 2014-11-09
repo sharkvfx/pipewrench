65927e387a69fd5a48750349995611c8d3a57ffc
Michael Hatton <michael@Shark.local>
2014-11-09 16:38:44 -0500

    cleared fix for linux

--- deliveryRobot/createTestDir.sh
+++ deliveryRobot/createTestDir.sh
@@ -3,19 +3,11 @@
 # project, episode, sequence, shot, version
 #./project/episode/sequence/shot/Comp/publish/version
 
-#<<<<<<< HEAD
 PROJECT=`cat /dev/urandom | env LC_CTYPE=C tr -dc 'A-Z'|head -c 3`
 EPISODE=`cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9A-Z'|head -c 3`
 SEQUENCE=`cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9'|head -c 3`
 VERSION=`echo -n 0;cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9'|head -c 1`
 SHOT=`cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9'|head -c 3;echo 0`
-#=======
-#PROJECT=`cat /dev/random | env LC_CTYPE=C tr -dc 'A-Z'|head -c 3`
-#EPISODE=`cat /dev/random | env LC_CTYPE=C tr -dc '0-9A-Z'|head -c 3`
-#SEQUENCE=`cat /dev/random | env LC_CTYPE=C tr -dc '0-9'|head -c 3`
-#VERSION=`echo -n 00;cat /dev/random | env LC_CTYPE=C tr -dc '0-9'|head -c 1`
-#SHOT=`cat /dev/random | env LC_CTYPE=C tr -dc '0-9'|head -c 3;echo 0`
-#>>>>>>> 0f5a4430cbda5470c1b4afba9d752c73678426c4
 
 SHOTLONG=$EPISODE-$SEQUENCE-$SHOT
 
