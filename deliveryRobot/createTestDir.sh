5bea1a9184d192234674b6a4a5ae8b78dc80f970
sharkvfx <sharkvfx@gmail.com>
2014-11-09 16:27:57 -0500

    updated for linux version

--- deliveryRobot/createTestDir.sh
+++ deliveryRobot/createTestDir.sh
@@ -3,11 +3,11 @@
 # project, episode, sequence, shot, version
 #./project/episode/sequence/shot/Comp/publish/version
 
-PROJECT=`cat /dev/random | env LC_CTYPE=C tr -dc 'A-Z'|head -c 3`
-EPISODE=`cat /dev/random | env LC_CTYPE=C tr -dc '0-9A-Z'|head -c 3`
-SEQUENCE=`cat /dev/random | env LC_CTYPE=C tr -dc '0-9'|head -c 3`
-VERSION=`echo -n 0;cat /dev/random | env LC_CTYPE=C tr -dc '0-9'|head -c 1`
-SHOT=`cat /dev/random | env LC_CTYPE=C tr -dc '0-9'|head -c 3;echo 0`
+PROJECT=`cat /dev/urandom | env LC_CTYPE=C tr -dc 'A-Z'|head -c 3`
+EPISODE=`cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9A-Z'|head -c 3`
+SEQUENCE=`cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9'|head -c 3`
+VERSION=`echo -n 0;cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9'|head -c 1`
+SHOT=`cat /dev/urandom | env LC_CTYPE=C tr -dc '0-9'|head -c 3;echo 0`
 SHOTLONG=$EPISODE-$SEQUENCE-$SHOT
 
 DIR="./test/$PROJECT/$EPISODE/$SEQUENCE/$SHOTLONG/Comp/publish/renders/$SHOTLONG_Comp_$VERSION/"
